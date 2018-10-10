98565e1779b3c7a3d6ed71011739c1d9beac9d6f
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2018-10-10 13:05:20 +0200

    Añadidas (algunas) clases

--- SuperSplinterCell/src/js/play_scene.js
+++ SuperSplinterCell/src/js/play_scene.js
@@ -9,3 +9,31 @@
 };
 
 module.exports = PlayScene;
+
+var Snake = {
+  hidden: true, 
+  kills: 0, 
+  position:{x:0,y:0}, 
+  magneticKey: false, 
+  documents: false,
+  items:{lockPick:null,taser:null,cable:null,takeObject:null,shoot:null},
+  movement : null
+};
+var Enemy = {
+  position: {x:0,y:0},
+  reducedVision: false,
+  awake: false,
+  detected: false,
+  movement:{guard1:null,guard2:null,guard3:null}                                                                                                         
+};
+var level = {
+  lightsOn : true,
+  objects: {
+    documents:{position: {x:0,y:0},taken:false},
+    magneticKey:{position:{x:0,y:0},taken:false} 
+  },
+  win : false,
+};
+
+var rooms = {};
+var doors = {};
