22ca692c309842ed3e38c8672e4eb817552a1ad2
ErGuille33 <37537142+ErGuille33@users.noreply.github.com>
2018-11-19 19:00:08 +0100

    Anim en clase y tilemaps

--- SuperSplinterCell/src/js/player.js
+++ SuperSplinterCell/src/js/player.js
@@ -2,10 +2,10 @@
 
 var Character = require('./character.js')
 
-function Player(x, y, key, doc, lp, t, c, g,h, sprite, game) {
+function Player(x, y, key, doc, lp, t, c, g, h, sprite, game) {
     Character.call(this, game, x, y, sprite);
     this.isMoving = false;
-    this.position.x = x; 
+    this.position.x = x;
     this.position.y = y;
     this.magneticKey = key;
     this.documents = doc;
@@ -22,11 +22,11 @@ Player.prototype.ini = function () {
     this.game.physics.arcade.enable(this);
     this.animations.add('idle', [0], 1, true);
     this.animations.add('walk', [11, 12, 13, 14], 10, true);
-    this.animations.add('taser', [0,1,2], 10, false)
-    this.animations.add('gun', [22,23,24,25], 10, false)
-    this.animations.add('hand', [3,4,5], 4, false)
-    this.animations.add('dead', [31,32], 1, false)
-    
+    this.animations.add('taser', [0, 1, 2], 10, false);
+    this.animations.add('gun', [22, 23, 24, 25], 10, false);
+    this.animations.add('hand', [3, 4, 5], 4, false);
+    this.animations.add('dead', [31, 32], 1, false);
+
     this.xDestine = this.x;
     this.yDestine = this.y;
     this.distance = 0;
@@ -47,6 +47,7 @@ Player.prototype.moveCharacter = function (layer) {
         this.yDestine = this.game.input.y;
         this.distance = Math.sqrt(Math.pow(this.xDestine - this.x, 2) + Math.pow(this.yDestine - this.y, 2));
         this.rotation = this.game.physics.arcade.moveToXY(this, this.game.input.x, this.game.input.y, this.speed);
+        this.animations.play('walk');
     }
     this.distance = Math.sqrt(Math.pow(this.xDestine - this.x, 2) + Math.pow(this.yDestine - this.y, 2));
     if (this.distance <= this.speed / this.game.time.physicsElapsedMS) { // una constante o variable (algo qe sea el incremento de movimiento)
@@ -63,10 +64,14 @@ Player.prototype.moveCharacter = function (layer) {
     }
 }
 Player.prototype.recogeInput = function () {
+
     if (this.actionButton.justDown) {
+        this.body.velocity.setTo(0, 0);
+        this.animations.stop();
         this.animations.play('gun');
-       this.shoot(); 
+        this.shoot();
     }
+
 }
 Player.prototype.shoot = function () {
     this.weapon.fire();
@@ -74,5 +79,8 @@ Player.prototype.shoot = function () {
 Player.prototype.update = function (layer) {
     this.moveCharacter(layer);
     this.recogeInput();
+}
+Player.prototype.stopAnimation = function () {
+
 }
 module.exports = Player;
