1e7848a58a4465f89170933bcf0500fcbce81936
Alberto Reymond González <areymond@ucm.es>
2018-12-09 20:25:16 +0100

    Enemigos v2
    Enemigos perfeccionados

--- SuperSplinterCell/src/js/Enemy.js
+++ SuperSplinterCell/src/js/Enemy.js
@@ -4,15 +4,11 @@ var Character = require('./character.js')
 var Player = require('./player.js')
 var Hand = require('./VisionCone.js');
 
-//Items : 1 = lockpick | 2 = taser | 3 = cable | 4 = gun  | 5 = hand 
-function Enemy(x, y, ko, look, posiciones, numPositions, speed, sprite, game) {
+function Enemy(x, y, ko, look, posiciones, speed, sprite, game) {
     Character.call(this, game, x, y, sprite);
     this.ko = false;
     this.look = false;
     this.game = game;
-    this.maxPositions = numPositions;
-    this.positions = posiciones;
-
 }
 Enemy.prototype = Object.create(Character.prototype);
 enemy.prototype.constructor = Enemy;
@@ -26,10 +22,7 @@ Enemy.prototype.ini = function () {
     this.xDestine = this.x;
     this.yDestine = this.y;
     this.sPeed = speed;
-
-    for (var i = 0; i < this.maxPositions; i++) {
-        this.positions.push({ x:  , y: p});
-    }
+    this.auxi = 0;
 
     this.coneOfVision = new coneOfVision(this.game, 20, 0, 'aux');
     this.coneOfVision.ini();
@@ -40,25 +33,29 @@ Enemy.prototype.ini = function () {
 
 Enemy.prototype.moveEnemy = function () {
 
-    if (!this.ko) {
-        //this.xDestine = ;
-        //this.yDestine = nextDestination.;
-        this.moveToXY(this.Enemy, this.xDestine,this.yDestine, this.speed)
-        if (look == false) this.rotation = this.game.physics.arcade.moveToXY(this, this.positions[1].x, this.positions[1].y, speed);
-        else this.rotation = this.game.physics.arcade.moveToXY(this, Player.x, Player.y, speed);
-        //console.log(this.xDestine );
-        this.animations.play('walk');
+    if (!this.ko && !this.look) {
+        if (this.x != posiciones[auxi].x && this.y != posiciones[auxi].x) {
+            this.xDestine = posiciones[i].x;
+            this.yDestine = posiciones[i].y;
+            this.moveToXY(this.Enemy, this.xDestine, this.yDestine, this.speed)
+            if (look == false) this.rotation = this.game.physics.arcade.moveToXY(this, this.positions[1].x, this.positions[1].y, this.speed);
+            else this.rotation = this.game.physics.arcade.moveToXY(this, Player.x, Player.y, this.speed);
+            //this.animations.play('walk');}
+        } else if (auxi < posiciones.length) {
+            auxi++;
+        } else auxi = 0;
+    } else {
+        this.body.velocity.setTo(0, 0);
+        //this.animations.play('KO');
     }
 }
-
-
 
 Enemy.prototype.gotHit = function () {
     if (this.checkOverlap(this.sprite, 'bullet')) this.ko = true;
 }
 
-Enemy.prototype.playerDetected = function(){
-    if (this.checkOverlap('aux','player')) look = true;
+Enemy.prototype.playerDetected = function () {
+    if (this.checkOverlap('aux', 'player')) look = true;
 }
 
 Enemy.prototype.checkOverlap = function (spriteA, spriteB) {
@@ -69,3 +66,10 @@ Enemy.prototype.checkOverlap = function (spriteA, spriteB) {
     return Phaser.Rectangle.intersects(boundsA, boundsB);
 }
 
+Enemy.prototype.update = function () {
+    this.playerDetected();
+    this.gotHit();
+    this.moveEnemy();
+}
+
+module.exports = Enemy;
--- SuperSplinterCell/src/js/VisionCone.js
+++ SuperSplinterCell/src/js/VisionCone.js
@@ -1,17 +1,16 @@
 'use strict';
 var Character = require('./character.js');
 
-function coneOfVision(game, x, y, sprite)
-{
-    Character.call(this,game,x,y,sprite);
-} 
+function coneOfVision(game, x, y, sprite) {
+    Character.call(this, game, x, y, sprite);
+}
 coneOfVision.prototype = Object.create(Character.prototype);
 coneOfVision.prototype.constructor = coneOfVision;
 
 coneOfVision.prototype.ini = function () {
     this.game.add.existing(this);
     this.game.physics.arcade.enable(this);
-    this.scale.setTo(1,1);
+    this.scale.setTo(1, 1);
     this.alpha = .1;
 }
 
