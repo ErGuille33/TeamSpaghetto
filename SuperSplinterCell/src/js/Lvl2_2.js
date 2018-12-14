b27b358c5ac5d3583d25c160215267b782c1dd1d
AreymondUcm <38209175+AreymondUcm@users.noreply.github.com>
2018-12-14 17:52:55 +0100

    Revert "Rehecho nivel 2"
    This reverts commit d88db438fe331f15da13449fb663633dd92e99bd.

--- SuperSplinterCell/src/js/Lvl2_2.js
+++ SuperSplinterCell/src/js/Lvl2_2.js
@@ -4,11 +4,11 @@ var Player = require('./player.js');
 //Esto viene a ser el objeto que contiene el juego(algo así como el game manager pero que controla todo)
 var Map = require('./map.js');
 var tspr = require('./triggerSprite.js');
-var documents = require('./documentos.js');
+var tarjetaLlave = require('./tarjetaLlave.js');
 //UI
 var Interface = require('./Interface.js');
 
-var Lvl2_1 = {
+var Lvl2_2 = {
   //Se ejecuta al principio
   create: function () {
     //Añadimos al mapa nuestro mapa ya cargado, y lo cargamos en la escena
@@ -22,35 +22,35 @@ var Lvl2_1 = {
     this.Sam;
     //Capas
    
-    this.map1 = new Map('Lvl2_2_1',48,48,'suelo',this,false,50,30);
+    this.map1 = new Map('Lvl2_1_1',48,48,'suelo',this,false,55,40);
     this.map1.ini();
    
-    this.map2 = new Map('Lvl2_2_2',48,48,'paredes',this,false,50,30);
+    this.map2 = new Map('Lvl2_1_2',48,48,'paredes',this,false,55,40);
     this.map2.ini();
 
-    this.map3 = new Map('Lvl2_2_3',48,48,'paredes',this,false,50,30);
+    this.map3 = new Map('Lvl2_1_3',48,48,'paredes',this,false,55,40);
     this.map3.ini();
     this.map3.collisions(0,1000);
 
-    this.map4 = new Map('Lvl2_2_4',48,48,'objetos',this,false,50,30);
+    this.map4 = new Map('Lvl2_1_4',48,48,'objetos',this,false,55,40);
     this.map4.ini();
     this.map4.collisions(0,1000);
     
 
-    this.map5 = new Map('Lvl2_2_5',48,48,'objetos',this,false,50,30);
+    this.map5 = new Map('Lvl2_1_5',48,48,'objetos',this,false,55,40);
     this.map5.ini();
 
-    this.map6 = new Map('Lvl2_2_6',48,48,'paredes',this,true,50,30);
+    this.map6 = new Map('Lvl2_1_6',48,48,'paredes',this,true,55,40);
     this.map6.ini();
     this.map6.collisions(0,1000);
 
-    //Items
-    this.docums = new documents (this.game,1025, 455, 'documents',.15,.15);
-    this.docums.ini();
+    //Tarjeta llave
+    this.magKey = new tarjetaLlave(this.game,2510,320,'tarjet',.075,.075);
+    this.magKey.ini();
 
     //Inicializamos el personaje
-    //154, 636
-    this.Sam = new Player(154, 636, true, false, 5, 'player', this.game);
+    //2305, 1743
+    this.Sam = new Player(2305, 1743, false, false, 5, 'player', this.game);
     this.game.add.existing(this.Sam);
     this.Sam.ini();
 
@@ -63,29 +63,22 @@ var Lvl2_1 = {
     //Cámara
     this.camera.follow(this.Sam);
 
-    this.nextLvl = new tspr(this.game, 2292, 1243, 'aux', .3,.4);
+    this.nextLvl = new tspr(this.game, 2453, 1745, 'aux', .2,1.3);
     this.nextLvl.ini();
-
-    this.endLvl = new tspr(this.game, 1444, 1437, 'aux', .8,.4);
-    this.endLvl.ini();
 
     this.checkIntersects = function(){
       if(Phaser.Rectangle.intersects(this.Sam,this.nextLvl)){
-        this.game.state.start('lvl2_2');
-      }
-      else if(Phaser.Rectangle.intersects(this.Sam,this.endLvl) && this.Sam.documents){
-        this.game.state.start('menu');
+        this.game.state.start('lvl2_1')
       }
     }
-    
   },
   //El update de toda la vida
   update: function () {
-    this.Sam.update(this.map4.returnLayer(),this.map3.returnLayer(), this.map6.returnLayer(),this.map6,undefined,this.docums);
+    this.Sam.update(this.map4.returnLayer(),this.map3.returnLayer(), this.map6.returnLayer(),this.map6,this.magKey,undefined);
     this.checkIntersects();
     this.interfaz.update(this.Sam.retDoc(), this.Sam.retKey());
   },
 };
 
-module.exports = Lvl2_1;
+module.exports = Lvl2_2;
 
